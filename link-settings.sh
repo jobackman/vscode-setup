af47cd819308cb495bc5ca9b89c0e3954bb189d8
Johan Bäckman <johan.c.backman@gmail.com>
2018-02-07 10:25:28 +0100

    Update link-settings.sh to remove existing destination files before linking

--- link-settings.sh
+++ link-settings.sh
@@ -4,6 +4,8 @@ function debug {
 
 function linkit {
   debug "Linking..."
+  rm ~/Library/Application\ Support/Code/User/settings.json
+  rm ~/Library/Application\ Support/Code/User/keybindings.json
   ln ./settings.json ~/Library/Application\ Support/Code/User/settings.json
   ln ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
   debug "Done!"
